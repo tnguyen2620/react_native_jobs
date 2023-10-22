f036781c11f1b7e7da2fb00178200c5cc01eb578
tnguyen2620 <114346285+tnguyen2620@users.noreply.github.com>
2023-10-22 23:11:59 +1100

    Get starting project to work

---
+++ app/index.js
@@ -0,0 +1,11 @@
+import {View, Text} from 'react-native';
+
+const Home = () => {
+    return (
+        <View>  
+            <Text>This shoudl be fun, </Text>
+        </View>
+    )
+}
+
+export default Home;
